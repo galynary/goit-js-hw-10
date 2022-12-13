dfaa662023ced18385acf7c61659ba96ee7dbc04
galynary <107271972+galynary@users.noreply.github.com>
2022-12-13 22:37:36 +0200

    change js

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
 export function showCountryList ({ flags, name }){
     return `
-    <li class = country-item>
+    <li class = country__item>
     <img class = 'country-list__flags' src="${flags.svg}" alt="${name.official}" width=50/>
     <h2 class = country-list__name>${name.official}</h2>
     </li>
@@ -10,11 +10,13 @@ export function showCountryList ({ flags, name }){
 export function showCountryCard({ flags, name, capital, population, languages }) {
     return `
     <div class="country">
-      <img class = "capital-flag" src="${flags.svg}" alt="${name.official}" width = 100/>
-      <h2 class = "country-title">Country: ${name.official}</h2>
-      <p class = "country-text">Capital: ${capital}</p>
-      <p class="country-text">Population: ${population}</p>
-      <p class="country-text">Languages: ${Object.values(languages)}</p>
+    <div class="country-info__wrapper">
+      <img class = "country-info__flags" src="${flags.svg}" alt="${name.official}" width = 100/>
+      <h2 class = "country-info__title">Country: ${name.official}</h2>
+      <p class = "country-info__text">Capital: ${capital}</p>
+      <p class="country-info__text">Population: ${population}</p>
+      <p class="country-info__text">Languages: ${Object.values(languages)}</p>
+      </div>
     </div>
     `
 }
