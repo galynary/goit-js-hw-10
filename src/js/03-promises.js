dc44b9f481396896d263cda83a47e0e9b079efc5
galynary <107271972+galynary@users.noreply.github.com>
2022-12-13 12:25:14 +0200

    create task 10

---
+++ src/js/03-promises.js
@@ -0,0 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onMakePromise);
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    timeId = setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+let timeId = null;
+function onMakePromise(e){  
+  e.preventDefault();
+  clearTimeout(timeId);
+  
+  let { delay, step, amount } = e.target.elements;
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  if (delay.value < 1 || step.value < 1 || amount.value < 1) {
+    Notify.failure(`All values must be greater than zero`);
+    return;
+  }
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise #${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise #${position} in ${delay}ms`);
+      });
+      delayValue += Number(step.value);
+  }
+  e.currentTarget.reset();
+}
+
